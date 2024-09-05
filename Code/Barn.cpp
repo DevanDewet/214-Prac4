b448ba5983ffa92f7e8383ba2c94a556f21d9bad
DevanDewet <161315233+DevanDewet@users.noreply.github.com>
2024-09-05 14:05:04 +0200

    updates

--- Code/Barn.cpp
+++ Code/Barn.cpp
@@ -1,1 +1,75 @@
-#include"Barn.h"
+#include "Barn.h"
+#include <iostream>
+
+// Constructor
+Barn::Barn(int storageCapacity, int trucksAvailable)
+    : storageCapacity(storageCapacity), trucksAvailable(trucksAvailable) {}
+
+
+void Barn::buyTruck() 
+{
+    trucksAvailable++;
+    std::cout << "Truck purchased! Total trucks available: " << trucksAvailable << "\n";
+}
+
+void Barn::sellTruck() {
+
+    if (trucksAvailable > 0) 
+    {
+        trucksAvailable--;
+        std::cout << "Truck sold! Total trucks available: " << trucksAvailable << "\n";
+    } 
+    else 
+    {
+        std::cout << "No trucks available to sell!\n";
+    }
+}
+
+int Barn::getTotalCapacity() 
+{
+    return storageCapacity;
+}
+
+std::string Barn::getCropType() 
+{
+    return "No crops stored in Barn."; 
+}
+
+std::string Barn::getSoilTypeName() 
+{
+    return "Barn does not have soil."; 
+}
+
+void Barn::harvest() {
+    std::cout << "Barn cannot harvest crops!\n"; 
+}
+
+int Barn::getLeftoverCapacity() 
+{
+    return storageCapacity; 
+}
+
+// Extra functionality for Barn
+void Barn::storeEquipment() 
+{
+    std::cout << "Storing equipment in the barn.\n";
+}
+
+void Barn::increaseStorageCapacity(int amount) 
+{
+    storageCapacity += amount;
+    std::cout << "Barn capacity increased by " << amount << ". New capacity: " << storageCapacity << "\n";
+}
+
+void Barn::dispatchTruck() {
+
+    if (trucksAvailable > 0) 
+    {
+        trucksAvailable--;
+        std::cout << "Truck dispatched. Trucks available: " << trucksAvailable << "\n";
+    } 
+    else 
+    {
+        std::cout << "No trucks available to dispatch!\n";
+    }
+}
--- Code/Barn.h
+++ Code/Barn.h
@@ -1,10 +1,39 @@
 #ifndef BARN_H
 #define BARN_H
 
-#include"FarmUnit.h"
+#include "FarmUnit.h"
+#include <string>
 
-class Barn : FarmUnit
-{
+class Barn : public FarmUnit {
+private:
+    int storageCapacity;
+    int trucksAvailable;
+
+public:
+    // Constructor
+    Barn(int storageCapacity, int trucksAvailable);
+
+    // Implementing FarmUnit methods
+    void buyTruck() override;
+
+    void sellTruck() override;
+
+    int getTotalCapacity() override;
+
+    std::string getCropType() override;
+
+    std::string getSoilTypeName() override;
+
+    void harvest() override;
+
+    int getLeftoverCapacity() override;
+
+    // Extra functionality for Barn
+    void storeEquipment();
+
+    void increaseStorageCapacity(int amount);
+
+    void dispatchTruck();
 };
 
 #endif
